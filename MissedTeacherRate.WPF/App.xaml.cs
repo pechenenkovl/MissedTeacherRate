cfd0e0495174c1fe9ac4cff97a9d0d22cd3745ac
Vladyslav Pechenenko <Vladyslav.Pechenenko@3shape.com>
2026-05-08 01:16:13 +0300

    Added general event handler

--- MissedTeacherRate.WPF/App.xaml.cs
+++ MissedTeacherRate.WPF/App.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
+﻿
 using System.Windows;
 
 namespace MissedTeacherRate.WPF
@@ -9,6 +8,21 @@ namespace MissedTeacherRate.WPF
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(
+                e.Exception.Message,
+                "Unexpected Error",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error
+            );
+            e.Handled = true;
+        }
     }
 
 }
